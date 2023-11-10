fe84f80f64f586045c53b33e0da76f53c0fe4108
Philoscope2023 <150458838+Philoscope2023@users.noreply.github.com>
2023-11-10 16:55:33 +0100

    Update flashcards.js

--- flashcards.js
+++ flashcards.js
@@ -1,24 +1,54 @@
-const flashcards = [
-    { front: "Qu'est-ce que la philosophie?", back: "La philosophie est l'amour de la sagesse." },
-    { front: "Qui est Socrate?", back: "Socrate était un philosophe grec célèbre pour sa méthode d'enseignement par questionnement." },
-    // Ajoutez autant de flashcards que nécessaire
+// Liste des cartes avec le recto et le verso
+const cards = [
+    { front: "Question 1", back: "Réponse 1" },
+    { front: "Question 2", back: "Réponse 2" },
+    // Ajoutez d'autres cartes au besoin
 ];
 
-let currentFlashcardIndex = 0;
+// Mélanger les cartes aléatoirement
+const shuffledCards = shuffleArray(cards);
 
-const frontFace = document.getElementById('front-face');
-const backFace = document.getElementById('back-face');
-const nextButton = document.getElementById('next-button');
+// Initialiser les piles de cartes
+let masteredCards = [];
+let toReviewCards = shuffledCards.slice(); // Copie des cartes mélangées
 
-nextButton.addEventListener('click', showNextFlashcard);
+// Sélectionner la première carte
+let currentCard = toReviewCards.pop();
+displayCard(currentCard);
 
-function showNextFlashcard() {
-    if (currentFlashcardIndex < flashcards.length) {
-        const flashcard = flashcards[currentFlashcardIndex];
-        frontFace.textContent = flashcard.front;
-        backFace.textContent = flashcard.back;
-        currentFlashcardIndex++;
+// Fonction pour afficher une carte
+function displayCard(card) {
+    document.getElementById('flashcard-front').textContent = card.front;
+    document.getElementById('flashcard-back').textContent = card.back;
+}
+
+// Fonction pour gérer le bouton "Suivant"
+function nextCard() {
+    // Déplacer la carte actuelle dans la pile appropriée
+    const isMastered = confirm("Avez-vous maîtrisé cette carte ?");
+    if (isMastered) {
+        masteredCards.push(currentCard);
     } else {
-        alert("Vous avez terminé toutes les flashcards!");
+        toReviewCards.unshift(currentCard);
     }
+
+    // Sélectionner la prochaine carte
+    if (toReviewCards.length > 0) {
+        currentCard = toReviewCards.pop();
+        displayCard(currentCard);
+    } else if (masteredCards.length > 0) {
+        alert("Vous avez maîtrisé toutes les cartes !");
+    } else {
+        alert("Vous avez révisé toutes les cartes !");
+    }
+}
+
+// Fonction pour mélanger un tableau aléatoirement
+function shuffleArray(array) {
+    const shuffled = array.slice();
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
 }
