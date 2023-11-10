6f18f55553c1e8576aef13eb0e7a87140400315a
Philoscope <150458838+Philoscope2023@users.noreply.github.com>
2023-11-10 19:21:33 +0100

    Update flashcards.js

--- flashcards.js
+++ flashcards.js
@@ -1,39 +1,90 @@
-const flashcards = [
-    { repere: "Intuition", definition: "Génie" },
-    { repere: "Discursif", definition: "Langage" },
-    { repere: "Médiat", definition: "Immédiat" }
-];
+$(document).ready(function () {
+    const concepts = [
+        { concept: 'intuitif', definition: 'discursif' },
+        { concept: 'médiat', definition: 'immédiat' },
+        { concept: 'nécessaire', definition: 'contingent' }
+    ];
 
-let currentIndex = 0;
+    let currentCardIndex = 0;
+    let knowCount = 0;
+    let reviewCount = 0;
 
-function flipCard() {
-    const card = document.querySelector('.flashcard');
-    card.classList.toggle('flipped');
-}
+    const card = $('#card');
+    const knowBtn = $('#knowBtn');
+    const nextBtn = $('#nextBtn');
+    const reviewBtn = $('#reviewBtn');
+    const knowCountSpan = $('#knowCount');
+    const reviewCountSpan = $('#reviewCount');
+    const resetBtn = $('#resetBtn');
+    const completionMessage = $('#completionMessage');
+    const conceptText = $('#conceptText');
+    const definitionText = $('#definitionText');
 
-function updateCard() {
-    const repereName = document.querySelector('.repere-name');
-    const repereDefinition = document.querySelector('.repere-definition');
+    function updateCard() {
+        const currentCard = concepts[currentCardIndex];
+        conceptText.text(currentCard.concept);
+        definitionText.text(currentCard.definition);
+    }
 
-    repereName.textContent = flashcards[currentIndex].repere;
-    repereDefinition.textContent = flashcards[currentIndex].definition;
-}
+    function resetCounts() {
+        knowCount = 0;
+        reviewCount = 0;
+        knowCountSpan.text(knowCount);
+        reviewCountSpan.text(reviewCount);
+    }
 
-function nextCard() {
-    const card = document.querySelector('.flashcard');
-    card.classList.remove('flipped');
-    currentIndex = (currentIndex + 1) % flashcards.length;
+    function showCompletionMessage() {
+        completionMessage.show();
+    }
+
+    function hideCompletionMessage() {
+        completionMessage.hide();
+    }
+
+    function showNextCard() {
+        if (currentCardIndex < concepts.length - 1) {
+            currentCardIndex++;
+            updateCard();
+            hideCompletionMessage();
+        } else {
+            showCompletionMessage();
+        }
+    }
+
+    function updateCounters() {
+        knowCountSpan.text(knowCount);
+        reviewCountSpan.text(reviewCount);
+    }
+
+    function knowButtonClick() {
+        knowCount++;
+        updateCounters();
+        showNextCard();
+    }
+
+    function reviewButtonClick() {
+        reviewCount++;
+        updateCounters();
+        showNextCard();
+    }
+
+    function resetButtonClick() {
+        currentCardIndex = 0;
+        resetCounts();
+        updateCard();
+        hideCompletionMessage();
+    }
+
+    card.on('click', function () {
+        card.toggleClass('flipped');
+    });
+
+    knowBtn.on('click', knowButtonClick);
+    nextBtn.on('click', showNextCard);
+    reviewBtn.on('click', reviewButtonClick);
+    resetBtn.on('click', resetButtonClick);
+
+    // Initial setup
     updateCard();
-}
-
-function knowCard() {
-    // Implement your logic to handle knowing the card
-}
-
-function resetCards() {
-    // Implement your logic to reset the flashcards
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    updateCard();
+    resetCounts();
 });
