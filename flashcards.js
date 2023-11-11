388e7d6d6d022b0d19bc5ebd78cd96a1b15a2381
Philoscope <150458838+Philoscope2023@users.noreply.github.com>
2023-11-11 16:07:58 +0100

    Update flashcards.js

--- flashcards.js
+++ flashcards.js
@@ -1,13 +1,24 @@
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
 $(document).ready(function () {
     const concepts = [
-        { concept: 'intuitif', definition: 'discursif' },
-        { concept: 'médiat', definition: 'immédiat' },
-        { concept: 'nécessaire', definition: 'contingent' }
+        { concept: 'Essentiel', definition: 'Est essentiel ce qui relève de l’essence, c’est-à-dire de la nature, ou encore de la définition d’une chose.On parle de propriété essentielle au sujet d’une propriété qui, si on l’enlève, fait disparaître le sujet dont elle est la propriété. Ce sont des propriétés indispensables.' },
+        { concept: 'Accidentel', definition: 'Est accidentel tout ce qui n’appartient à pas à l’essence d’une chose ; ce qui peut être ou ne pas être sans que cela ne change le sujet. Ce ne sont pas des propriétés indispensables.' },
+        { concept: 'Nécessaire', definition: 'Est nécessaire ce qui ne peut pas ne pas exister, ou être différent. Un événement est nécessaire quand il est déterminé par une série de causes et d’effets. C’est ce dont le contraire est impossible.' },
+        { concept: 'Contingent', definition: 'Est contingent ce qui peut ne pas exister, ou être différent. Un événement est contingent quand il est déterminé par des circonstances particulières. C’est ce dont le contraire est possible.' }
     ];
 
     let currentCardIndex = 0;
     let knowCount = 0;
     let reviewCount = 0;
+    let revisedCount = 0;
+    let evaluatedCards = [];
 
     const card = $('#card');
     const knowBtn = $('#knowBtn');
@@ -15,6 +26,7 @@ $(document).ready(function () {
     const reviewBtn = $('#reviewBtn');
     const knowCountSpan = $('#knowCount');
     const reviewCountSpan = $('#reviewCount');
+    const revisedCountSpan = $('#revisedCount');
     const resetBtn = $('#resetBtn');
     const completionMessage = $('#completionMessage');
     const conceptText = $('#conceptText');
@@ -35,56 +47,95 @@ $(document).ready(function () {
 
     function showCompletionMessage() {
         completionMessage.show();
-    }
+        createConfetti();
+    }    
+    
+    function createConfetti() {
+        const duration = 10 * 1000; // durée de l'explosion en millisecondes (10 secondes ici)
+        const { clientWidth, clientHeight } = document.documentElement;
+    
+        const confettiSettings = { target: 'confetti-canvas', clock: duration };
+    
+        const confetti = new ConfettiGenerator.default(confettiSettings);
+        confetti.render();
+    
+        setTimeout(() => {
+            confetti.clear();
+            console.log('Confetti cleared.'); // Ajout d'un log pour le débogage
+        }, duration);
+    }        
 
     function hideCompletionMessage() {
         completionMessage.hide();
     }
 
     function showNextCard() {
-        if (currentCardIndex < concepts.length - 1) {
-            currentCardIndex++;
-            updateCard();
-            hideCompletionMessage();
-        } else {
-            showCompletionMessage();
+        if (!completionMessage.is(':visible')) {
+            if (currentCardIndex < concepts.length - 1) {
+                currentCardIndex++;
+                updateCard();
+                hideCompletionMessage();
+                evaluatedCards = [];
+                revisedCountSpan.text(++revisedCount);
+            } else {
+                // Vérifier si toutes les cartes ont été évaluées
+                if (evaluatedCards.length === concepts.length) {
+                    // Ne rien faire ici, car le message de complétion a déjà été affiché
+                } else {
+                    // Incrémenter le compteur une dernière fois lorsque le message de complétion s'affiche
+                    revisedCountSpan.text(++revisedCount);
+                    showCompletionMessage();
+                }
+            }
+            // Assurez-vous que la carte affichée est toujours un recto
+            card.removeClass('flipped');
         }
     }
-
+    
     function updateCounters() {
         knowCountSpan.text(knowCount);
         reviewCountSpan.text(reviewCount);
     }
 
-    function knowButtonClick() {
-        knowCount++;
-        updateCounters();
-        showNextCard();
+    function handleReviewButtonClick() {
+        if (!evaluatedCards.includes(currentCardIndex)) {
+            reviewCount++;
+            evaluatedCards.push(currentCardIndex);
+            updateCounters();
+        }
     }
 
-    function reviewButtonClick() {
-        reviewCount++;
-        updateCounters();
-        showNextCard();
+    function handleKnowButtonClick() {
+        if (!evaluatedCards.includes(currentCardIndex)) {
+            knowCount++;
+            evaluatedCards.push(currentCardIndex);
+            updateCounters();
+        }
     }
 
     function resetButtonClick() {
         currentCardIndex = 0;
+        shuffleArray(concepts);
         resetCounts();
+        revisedCount = 0;
+        revisedCountSpan.text(revisedCount);
         updateCard();
         hideCompletionMessage();
+        card.removeClass('flipped');
+        evaluatedCards = [];
     }
 
     card.on('click', function () {
         card.toggleClass('flipped');
     });
 
-    knowBtn.on('click', knowButtonClick);
+    knowBtn.on('click', handleKnowButtonClick);
     nextBtn.on('click', showNextCard);
-    reviewBtn.on('click', reviewButtonClick);
+    reviewBtn.on('click', handleReviewButtonClick);
     resetBtn.on('click', resetButtonClick);
 
     // Initial setup
+    shuffleArray(concepts);
     updateCard();
     resetCounts();
 });
